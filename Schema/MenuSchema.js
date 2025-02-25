537cf62c868092a01d8321259dc6fe65021c1fc9
Kunal Deotale <kdeotale4@gmail.com>
2025-02-25 21:04:53 +0530

    update user and menuitem file

--- Schema/MenuSchema.js
+++ Schema/MenuSchema.js
@@ -1,11 +1,20 @@
-import mongoose from "mongoose";
+// Schema definition for menu items in the application
+const mongoose = require("mongoose");
 
+/**
+ * Schema definition for a menu item.
+ * 
+ * @typedef {Object} MenuItem
+ * @property {string} name - The name of the menu item. This field is required.
+ * @property {string} [description=""] - A description of the menu item. Defaults to an empty string.
+ * @property {string} category - The category of the menu item. Must be one of "coffee", "desserts", or "beverages".
+ * @property {string} imageUrl - The URL of the image representing the menu item. Must be a valid URL starting with http or https.
+ */
 const menuItemSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    description: { type: String },
-    price: { type: Number, required: true },
-    category: { type: String, enum: ["Coffee", "Desserts", "Beverages"] },
-    imageUrl: { type: String },
+    description: { type: String, default: "" },
+    category: { type: String, enum: ["coffee", "desserts", "beverages"] },
+    imageUrl: { type: String, match: /^https?:\/\/.+/ },
 });
 
 module.exports = mongoose.model("MenuItem", menuItemSchema);
--- Schema/UserSchema.js
+++ Schema/UserSchema.js
@@ -1,6 +1,16 @@
 
 import mongoose from "mongoose";
 
+/**
+ * User Schema for MongoDB using Mongoose.
+ * 
+ * @typedef {Object} UserSchema
+ * @property {string} username - The unique username of the user. Required.
+ * @property {string} password - The password of the user. Required.
+ * @property {string} email - The unique email address of the user. Required.
+ * @property {string} name - The name of the user. Required.
+ * @property {number} phone - The phone number of the user. Required.
+ */
 let userSchema = new mongoose.Schema({
     username: { type: String, required: true,unique: true },
     password: { type: String, required: true },
